815136713ad5602cc560afafadb409287c9a44a5
serii burduja <seriiburduja@gmail.com>
2025-03-27 16:52:55 +0200

    feat: comment web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::any('{any}', function () {
-    redirect('/');
-})->where('any', '.*');
+// Route::any('{any}', function () {
+//     redirect('/');
+// })->where('any', '.*');

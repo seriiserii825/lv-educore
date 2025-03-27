ce75b79fb6f43b3ea2fa91dba998a3d409950fb5
serii burduja <seriiburduja@gmail.com>
2025-03-27 17:35:46 +0200

    feat: finished sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,19 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function(){
+Route::get('/home', function () {
     return response()->json(['message' => 'Hello World!'], 200);
+});
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
 
 Route::get('/user', function (Request $request) {

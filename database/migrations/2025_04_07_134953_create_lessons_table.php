f2517c12da0c7d6cd50ecbd144226eb891df347b
serii burduja <seriiburduja@gmail.com>
2025-04-07 16:55:10 +0300

    feat: create lesson migration

---
+++ database/migrations/2025_04_07_134953_create_lessons_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lessons', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->unique();
+            $table->string('slug')->unique();
+            $table->text('description');
+            $table->foreignId('instructor_id')->constrained('users');
+            $table->foreignId('course_id')->constrained('courses')->onDelete('cascade');
+            $table->foreignId('chapter_id')->constrained('course_chapters')->onDelete('cascade');
+            $table->string('file_path')->nullable();
+            $table->enum('storage', ['upload', 'youtube', 'vimeo', 'external_link'])->default('upload');
+            $table->enum('file_type', ['video', 'audio', 'text', 'pdf'])->default('video');
+            $table->integer('volume')->default(0);
+            $table->integer('duration')->default(0);
+            $table->boolean('downloadable')->default(0);
+            $table->integer('order')->default(0);
+            $table->boolean('is_preview')->default(0);
+            $table->boolean('status')->default(1);
+            $table->enum('lesson_type', ['lesson', 'live'])->default('lesson');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lessons');
+    }
+};

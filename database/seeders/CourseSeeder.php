ffaa5eaef42eedf1a7792dbc4dd4508a53e9d748
serii burduja <seriiburduja@gmail.com>
2025-04-08 08:47:30 +0300

    feat: course seeder

--- database/seeders/CourseSeeder.php
+++ database/seeders/CourseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Course;
 use Illuminate\Database\Seeder;
 
 class CourseSeeder extends Seeder
@@ -13,5 +13,28 @@ class CourseSeeder extends Seeder
     public function run(): void
     {
         //
+        $courses = [
+            [
+                'title' => 'Course Title',
+                'slug' => 'course-title',
+                'seo_description' => 'Course SEO Description',
+                'thumbnail' => 'thumbnail.jpg',
+                'demo_video_storage' => 'youtube',
+                'demo_video_source' => 'demo_video.mp4',
+                'price' => 100,
+                'discount' => 10,
+                'description' => 'Course Description',
+                'category_id' => 1,
+                'instructor_id' => 2,
+                'course_level_id' => 1,
+                'course_language_id' => 1,
+            ]
+        ];
+
+        foreach ($courses as $course) {
+            $new_course = new Course();
+            $new_course = $new_course->fill($course);
+            $new_course->save();
+        }
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
             CourseLanguageSeeder::class,
             CourseLevelSeeder::class,
             CourseCategorySeeder::class,
+            CourseSeeder::class,
         ]);
     }
 }

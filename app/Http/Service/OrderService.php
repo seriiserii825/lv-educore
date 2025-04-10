b10d4539d812f63f50c034865149142a2689498f
serii burduja <seriiburduja@gmail.com>
2025-04-10 22:14:33 +0300

    feat: Order service

---
+++ app/Http/Service/OrderService.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Service;
+
+use App\Models\Order;
+
+class OrderService
+{
+
+    function storeOrder(
+        int $buyer_id,
+        int $seller_id,
+        string $status,
+        float $total_amount,
+        float $paid_amount,
+        string $transaction_id,
+        string $payment_method
+    ) {
+        $order = new Order();
+        $order->invoice_id = uniqid();
+        $order->buyer_id = $buyer_id;
+        $order->seller_id = $seller_id;
+        $order->status = $status;
+        $order->total_amount = $total_amount;
+        $order->paid_amount = $paid_amount;
+        $order->transaction_id = $transaction_id;
+        $order->payment_method = $payment_method;
+
+        $order->save();
+    }
+}

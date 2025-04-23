7cae1b013be57e647bed3175b654ee556915aa1c
serii burduja <seriiburduja@gmail.com>
2025-04-23 13:31:05 +0300

    feat: lesson description

--- database/migrations/2025_04_07_134953_create_lessons_table.php
+++ database/migrations/2025_04_07_134953_create_lessons_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title')->unique();
             $table->string('slug')->unique();
+            $table->string('description')->nullable();
             $table->foreignId('instructor_id')->constrained('users');
             $table->foreignId('course_id')->constrained('courses')->onDelete('cascade');
             $table->foreignId('chapter_id')->constrained('course_chapters')->onDelete('cascade');

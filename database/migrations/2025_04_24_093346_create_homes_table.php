2bbcfc5e9c10345f0c311d5d17e7db3ec1758275
serii burduja <seriiburduja@gmail.com>
2025-04-25 10:34:14 +0300

    feat: heroes homes

--- database/migrations/2025_04_24_093346_create_homes_table.php
+++ database/migrations/2025_04_24_093346_create_homes_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('heroes', function (Blueprint $table) {
+        Schema::create('homes', function (Blueprint $table) {
             $table->id();
             $table->string('label');
             $table->string('title');
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('heroes');
+        Schema::dropIfExists('homes');
     }
 };

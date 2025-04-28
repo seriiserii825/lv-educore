e2609911c137e7fb3c846678a539507fadf78082
serii burduja <seriiburduja@gmail.com>
2025-04-28 07:23:58 +0300

    feat: fillable to guarded

--- app/Models/Home.php
+++ app/Models/Home.php
@@ -6,16 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Home extends Model
 {
-    protected $fillable = [
-        'label',
-        'title',
-        'description',
-        'video_text',
-        'video_url',
-        'button_text',
-        'banner_title',
-        'banner_text',
-        'round_text',
-        'image'
-    ];
+    protected $guarded = ['id'];
 }
